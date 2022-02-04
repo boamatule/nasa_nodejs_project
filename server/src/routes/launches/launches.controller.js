c3a973a7bd9bc32543ac03437b7f21fd62dee285
boa <boa.matule@gmail.com>
2022-02-04 17:30:35 +0100

    Creates functions to abort the launching

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -1,31 +1,56 @@
-const { getAllLaunches, addNewLaunch } = require("../../models/launches.model");
+const {
+	getAllLaunches,
+	addNewLaunch,
+	existsLaunchWithId,
+	abortLaunchById,
+} = require("../../models/launches.model");
 
 function httpGetAllLaunches(req, res) {
-  return res.status(200).json(getAllLaunches());
-  // return res.status(200).json(launches.value());
-  // return res.status(200).json(Array.from(launches.value()));
+	return res.status(200).json(getAllLaunches());
+	// return res.status(200).json(launches.value());
+	// return res.status(200).json(Array.from(launches.value()));
 }
 
 function httpAddNewLaunch(req, res) {
-  const launch = req.body;
+	const launch = req.body;
 
-  if (
-    !launch.mission || !launch.rocket || !launch.launchDate || !launch.target
-  ) {
-    return res.status(400);
-  }
+	if (
+		!launch.mission ||
+		!launch.rocket ||
+		!launch.launchDate ||
+		!launch.target
+	) {
+		return res.status(400);
+	}
 
-  launch.launchDate = new Date(launch.launchDate);
-  if (isNaN(launch.launchDate)) {
-    return res.status(400).json({
-      error: "Invalid launch date",
-    });
-  }
-  addNewLaunch(launch);
-  return res.status(201).json(launch);
+	launch.launchDate = new Date(launch.launchDate);
+	if (isNaN(launch.launchDate)) {
+		return res.status(400).json({
+			error: "Invalid launch date",
+		});
+	}
+
+	addNewLaunch(launch);
+	return res.status(201).json(launch);
+}
+
+function httpAbortLaunch(req, res) {
+	const launchId = Number(req.params.id);
+
+	//if launch doesn't exist
+	if (!existsLaunchWithId(launchId)) {
+		return res.status(404).json({
+			error: "Launch not found",
+		});
+	}
+
+	//if launch does exist
+	const aborted = abortLaunchById(launchId);
+	return res.status(200).json(aborted);
 }
 
 module.exports = {
-  httpGetAllLaunches,
-  httpAddNewLaunch,
+	httpGetAllLaunches,
+	httpAddNewLaunch,
+	httpAbortLaunch,
 };

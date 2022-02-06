2e50ce6d4e46e2f404c6b3fb906eec3eb9fb9015
boa <boa.matule@gmail.com>
2022-02-06 13:35:19 +0100

    Creates planets.mongo file and defines a planetsSchema:

---
+++ server/src/models/planets.mongo.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const planetsSchema = new mongoose.Schema({
+	keplerName: {
+    type: String,
+    required: true,
+  }
+});
+
+
+// Connects planetsSchema with the "launches" collection
+module.exports = mongoose.model('Planet', planetsSchema);

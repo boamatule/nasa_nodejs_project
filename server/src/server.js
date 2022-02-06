8e51f1845f9a41745210d11d862a213870a48509
boa <boa.matule@gmail.com>
2022-02-06 13:16:32 +0100

    Creates a launches.mongo.js in our models folder and defines the launchesSchemma

--- server/src/server.js
+++ server/src/server.js
@@ -7,6 +7,7 @@ const { loadPlanetsData } = require("./models/planets.model");
 const PORT = process.env.PORT || 8000;
 
 const MONGO_URL =
+	"mongodb+srv://nasa-api:YyNFbfyRWz4oDSQz@nasacluster.6i1y4.mongodb.net/nasa?retryWrites=true&w=majority";
 
 const server = http.createServer(app);
 

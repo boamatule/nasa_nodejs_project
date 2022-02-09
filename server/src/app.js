2ab174a0a947a119121f21e6acfb4fe1a3bc1d92
boa <boa.matule@gmail.com>
2022-02-09 15:22:14 +0100

    Fix error

--- server/src/app.js
+++ server/src/app.js
@@ -2,14 +2,9 @@ const path = require("path");
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
-const helmet = require("helmet");
-
 
 const api = require("./routes/api");
 const app = express();
-
-app.use(helmet());
-
 
 app.use(
 	cors({

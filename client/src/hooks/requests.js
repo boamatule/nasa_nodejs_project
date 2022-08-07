29f4ee728d3a73a0de23b4c107fe105efe7fbe56
boa <boa.matule@gmail.com>
2022-08-07 20:19:12 +0200

    Updates Database Access Keys

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -4,17 +4,17 @@ const API_URL = "v1";
 
 // Load planets and return as JSON.
 async function httpGetPlanets() {
-	const response = await fetch(`${API_URL}/planets`);
-	return await response.json();
+  const response = await fetch(`${API_URL}/planets`);
+  return await response.json();
 }
 
 // Load launches, sort by flight number, and return as JSON.
 async function httpGetLaunches() {
-	const response = await fetch(`${API_URL}/launches`);
-	const fetchedLaunches = await response.json();
-	return fetchedLaunches.sort((a, b) => {
-		return a.flightNumber - b.flightNumber;
-	});
+  const response = await fetch(`${API_URL}/launches`);
+  const fetchedLaunches = await response.json();
+  return fetchedLaunches.sort((a, b) => {
+    return a.flightNumber - b.flightNumber;
+  });
 }
 
 // Submit given launch data to launch system.
@@ -36,16 +36,21 @@ async function httpSubmitLaunch(launch) {
 
 // Delete launch with given ID.
 async function httpAbortLaunch(id) {
-	try {
-		return await fetch(`${API_URL}/launches/${id}`, {
-			method: "delete",
-		});
-	} catch (error) {
-		console.log(error);
-		return {
-			ok: false,
-		};
-	}
+  try {
+    return await fetch(`${API_URL}/launches/${id}`, {
+      method: "delete",
+    });
+  } catch(err) {
+    console.log(err);
+    return {
+      ok: false,
+    };
+  }
 }
 
-export { httpGetPlanets, httpGetLaunches, httpSubmitLaunch, httpAbortLaunch };
+export {
+  httpGetPlanets,
+  httpGetLaunches,
+  httpSubmitLaunch,
+  httpAbortLaunch,
+};
